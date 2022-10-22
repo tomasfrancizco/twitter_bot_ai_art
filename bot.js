282f0681bcd65f1d06f08b5126d820603b2629a9
Tomas <tomasfreire@gmail.com>
2022-10-22 19:53:43 -0300

    filtering #waifudiffusion

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const config = require("./config")
 const Twitter = new twit(config);
 
 const retweet = function (offset) {
+
+  // console.log({stream})
   const params = {
     q: '#aiart', // REQUIRED
     result_type: 'recent',
@@ -11,14 +13,19 @@ const retweet = function (offset) {
   }
 
   Twitter.get('search/tweets', params, (err, data) => {
-    if(offset >= data.statuses.length) return;
+    if (offset >= data.statuses.length) return;
     // creates a list of tweets and sorts it from most to less engaged (fav & rt count)
     const tweets = data.statuses.map((tweet) => {
-      return tweet
+      let banned = false;
+      for(let i=0;i<tweet.entities.hashtags.length;i++){
+        let lowerText = tweet.entities.hashtags[i].text.toLowerCase();
+        if(lowerText === "waifudiffusion") banned = true;
+      }
+      return !banned ? tweet : null;
     }).sort((a, b) => {
       return (b.retweet_count + b.favorite_count) - (a.retweet_count + a.favorite_count)
-    })
-
+    });
+    
     if (!err) {
       // gets the list's top tweet
       const retweetId = tweets[offset].id_str;
@@ -28,11 +35,10 @@ const retweet = function (offset) {
         if (err && err.code === 327) {
           // if the tweet has already been retweeted by the bot, starts again with next index
           retweet(offset + 1)
-        } else if(err && err.code !== 327) {
+        } else if (err && err.code !== 327) {
           Twitter.post('statuses/update', {
-            status: `@tomasfrancisco h${Math.floor(Math.random() * 1337)}y!`
-          }, (err, res) => {}
-          )
+            status: `@tomasfrancizco h${Math.floor(Math.random() * 1337)}y!`
+          }, (err, res) => {})
         }
       })
     }
